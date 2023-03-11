46d060bd8b9078d45c92245a3b1e9f9b59140684
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-03-11 22:42:30 +0100

    Task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const imageCard = createImageCard(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imageCard);
+
+function createImageCard(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="#">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`;
+    })
+    .join("");
+}
+
+document.querySelector(".gallery").onclick = (e) => {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  basicLightbox
+    .create(
+      `
+		<img width="100%" height="100%" src="${e.target.dataset.source}">
+	`
+    )
+    .show();
+};
